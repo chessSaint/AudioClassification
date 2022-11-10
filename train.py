3bc048b3755ff7bb4ff1112db1e516e4c8ee31fb
chessSaint <1143347768@qq.com>
2022-11-10 20:18:24 +0800

    1110-ubantu

--- train.py
+++ train.py
@@ -21,7 +21,7 @@ from utils.utility import add_arguments, print_arguments
 parser = argparse.ArgumentParser(description=__doc__)
 add_arg = functools.partial(add_arguments, argparser=parser)
 add_arg('use_model',        str,    'ecapa_tdnn',             '所使用的模型')
-add_arg('batch_size',       int,    16,                       '训练的批量大小')
+add_arg('batch_size',       int,    64,                       '训练的批量大小')
 add_arg('num_workers',      int,    4,                        '读取数据的线程数量')
 add_arg('audio_duration',   float,  3,                        '训练的音频长度，单位秒')
 add_arg('min_duration',     float,  0.5,                      '训练的最短音频长度，单位秒')
@@ -76,6 +76,24 @@ def train(args):
                                   min_duration=args.min_duration,
                                   do_vad=False,
                                   augmentors=augmentors)
+    """ DataLoader用法
+        -　使用流程
+            ① 创建一个 Dataset 对象
+            ② 创建一个 DataLoader 对象
+            ③ 循环这个 DataLoader 对象
+        -　参数
+            dataset(Dataset): 传入的数据集
+            batch_size(int, optional): 每个batch有多少个样本
+            shuffle(bool, optional): 在每个epoch开始的时候，对数据进行重新排序
+            sampler(Sampler, optional): 自定义从数据集中取样本的策略，如果指定这个参数，那么shuffle必须为False
+            batch_sampler(Sampler, optional): 与sampler类似，但是一次只返回一个batch的indices（索引），需要注意的是，一旦指定了这个参数，那么batch_size,shuffle,sampler,drop_last就不能再制定了（互斥——Mutually exclusive）
+            num_workers (int, optional): 这个参数决定了有几个进程来处理data loading。0意味着所有的数据都会被load进主进程。（默认为0）
+            collate_fn (callable, optional): 将一个list的sample组成一个mini-batch的函数
+            pin_memory (bool, optional)： 如果设置为True，那么data loader将会在返回它们之前，将tensors拷贝到CUDA中的固定内存（CUDA pinned memory）中.
+            drop_last (bool, optional): 如果设置为True：这个是对最后的未完成的batch来说的，比如你的batch_size设置为64，而一个epoch只有100个样本，那么训练的时候后面的36个就被扔掉了…如果为False（默认），那么会继续正常执行，只是最后的batch_size会小一点。
+            timeout(numeric, optional): 如果是正数，表明等待从worker进程中收集一个batch等待的时间，若超出设定的时间还没有收集到，那就不收集这个内容了。这个numeric应总是大于等于0。默认为0
+            worker_init_fn (callable, optional): 每个worker初始化函数 If not None, this will be called on each
+    """
     train_loader = DataLoader(dataset=train_dataset, batch_size=args.batch_size, shuffle=True, collate_fn=collate_fn, num_workers=args.num_workers)
 
     test_dataset = CustomDataset(args.test_list_path,
@@ -124,6 +142,7 @@ def train(args):
     # 获取损失函数
     loss = torch.nn.CrossEntropyLoss()  # 交叉熵损失函数，刻画的是实际输出（概率）与期望输出（概率）的距离。https://zhuanlan.zhihu.com/p/98785902
 
+    # train_loader的长度是根据数据的总数和batch_size的大小共同计算出来的
     sum_batch = len(train_loader) * (args.num_epoch - last_epoch)
     # 开始训练
     for epoch in range(args.num_epoch):
@@ -131,6 +150,13 @@ def train(args):
         accuracies = []
         train_times = []
         start = time.time()
+
+        # enumerate() 函数用于将一个可遍历的数据对象(如列表、元组或字符串)组合为一个索引序列，同时列出数据和数据下标，一般用在 for 循环当中。
+        # 语法为　enumerate(sequence, [start=0])
+        """参数
+            sequence -- 一个序列、迭代器或其他支持迭代对象。
+            start -- 下标起始位置的值。
+        """
         for batch_id, (spec_mag, label) in enumerate(train_loader):
             spec_mag = spec_mag.to(device)
             label = label.to(device).long()
@@ -143,7 +169,26 @@ def train(args):
             optimizer.step()  # 更新参数
 
             # 计算准确率
-            output = torch.nn.functional.softmax(output, dim=-1)  # softmax() 将各个输出节点的输出值范围映射到[0, 1]，并且约束各个输出节点的输出值的和为1
+            # softmax() 将各个输出节点的输出值范围映射到[0, 1]，并且约束各个输出节点的输出值的和为1
+            # dim 0 - 对某一维度中的列进行softmax；1 - 对某一维度的行进行softmax；-1 - 对某一维度的行进行softmax
+            """
+                import numpy as np
+    
+                a = np.asarray([1, 2, 3, 4])
+                b = np.asarray([1, 5, 6, 7])
+                c = (a == b).astype(int)
+                mean = np.mean(c)
+                print(a)
+                print(b)
+                print(c)
+                print(mean)
+                
+                [1 2 3 4]
+                [1 5 6 7]
+                [1 0 0 0]
+                0.25
+            """
+            output = torch.nn.functional.softmax(output, dim=-1)
             output = output.data.cpu().numpy()
             output = np.argmax(output, axis=1)
             label = label.data.cpu().numpy()
@@ -157,7 +202,7 @@ def train(args):
                 print(f'[{datetime.now()}] Train epoch [{epoch}/{args.num_epoch}], batch: {batch_id}/{len(train_loader)}, '
                       f'lr: {scheduler.get_last_lr()[0]:.8f}, loss: {sum(loss_sum) / len(loss_sum):.8f}, '
                       f'accuracy: {sum(accuracies) / len(accuracies):.8f}, '
-                      f'eta: {eta_str}')
+                      f'eta: {eta_str}')  # eta: Estimated Time of Arrival - 预计到达时间
             start = time.time()
         scheduler.step()
         # 评估模型
@@ -166,6 +211,10 @@ def train(args):
         print(f'[{datetime.now()}] Test {epoch}, Accuracy: {acc}')
         print('='*70)
         # 保存模型
+        """torch.save
+            - torch.save(model,'net.pth') 保存加载整个模型（不推荐）
+            - torch.save(model.state_dict(),'net_params.pth') 只保存加载模型参数
+        """
         os.makedirs(args.save_model_dir, exist_ok=True)
         torch.save(model.state_dict(), os.path.join(args.save_model_dir, 'model.pth'))
         torch.save({'last_epoch': torch.tensor(epoch)}, os.path.join(args.save_model_dir, 'model.state'))
